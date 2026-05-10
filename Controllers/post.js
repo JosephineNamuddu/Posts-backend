feb6ac11a5fdab2c747d1628e6aae6da2521b6d3
JosephineNamuddu <jorselyne@gmail.com>
2026-05-10 20:43:48 +0300

    updates

--- Controllers/post.js
+++ Controllers/post.js
@@ -12,7 +12,7 @@ const CreatePost = async (req, res) => {
 } catch (error) {
     res.status(500).json({message: 'Error while creating post', error: error.message});
         }
-}
+};
 
 //fetch/read posts
 const getPosts = async (req, res) => {
@@ -22,7 +22,7 @@ try {
 } catch (error) {
   res.status(500).json({message: 'Error while fetching posts', error: error.message});  
 }
-}
+};
 //updating APIs using a put request
 const updatePost = async (req, res) => {
     const { id } = req.params; 
@@ -45,8 +45,8 @@ const updatePost = async (req, res) => {
         } catch (error) {
         res.status(500).json({ message: 'Error while updating post',error: error.message})
         }
-    }
-//Patch method of updating APIs
+    };
+//1. Patch method of updating APIs
 const patchPost = async (req, res) => {
     const { id } = req.params; 
     const { title, content} = req.body;
@@ -70,23 +70,54 @@ const patchPost = async (req, res) => {
         res.status(500).json({ message: 'Error while updating post',error: error.message})
 
    }
-}
-//Deleting post
+};
+//2. Deleting post
 const deletePost = async (req,res) => {
-const Post = await Post.findById(req.params.id);
-if(Post.creator.toString()!== req.user._id.toString()){
-    return res.status(403).json({message:'Not authorized'});
+const {id} = req.params;
+try {
+    const existingPost = await Post.findById(id);
+
+    if (!existingPost) {
+        return res.status(404).json({message: 'Post not found'});
+    }
+    if(req.user._id.toString()!==existingPost.creator.toString()){
+        return res.status(403).json({message: 'Unauthorised, you are not the owner'});
+    }
+    await Post.findByIdAndDelete(id);
+    res.status(200).json({message: 'Post deleted successfully'});
+} catch (error) {
+ res.status(500).json({message: 'Error while deleting post', error: error.message}); 
 }
+};
+
+// 4. Get post by ID
+const getPostById = async (req, res) => {
+    const {id} = req.params;
 
     try {
-        await Post.findByIdAndDelete(req.params.id);
-        res.status(200).json({message:'Post deleted successfully'});
+        const post = await Post.findById(id);
+        if(!post) {
+            return res.status(404).json({message: 'Post not found'});
+        }
+        res.status(200).json({message:'Post fetched successfully', result:Post});
+
     } catch (error) {
-       res.status(500).json({message:error.message}); 
+      res.status(500).json({message:'Error while fetching Post', error: error.message});  
     }
-}
+};
+
+// 5. get all posts by a specific user
+const getPostsByUser = async(req, res) =>{
+    const{userId} = req.params;
+
+    try {
+        const Posts = await Post.find({creator: userId});
+        res.status(200).json({message:'Posts fetched successfully', result: Posts})
+
+    } catch (error) {
+        res.status(500).json({message: 'Error while fetching posts', error: error.message});
+    }
+};
 
 
-
-
-module.exports = {CreatePost,getPosts, updatePost, patchPost, deletePost }; //importing 
+module.exports = {CreatePost,getPosts, updatePost, patchPost, deletePost, getPostById, getPostsByUser }; //importing 
--- Routes/post.js
+++ Routes/post.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { CreatePost, getPosts, updatePost, patchPost,deletePost } = require('../Controllers/post');
+const { CreatePost, getPosts, updatePost, patchPost,deletePost, getPostById, getPostsByUser } = require('../Controllers/post');
 
 const auth = require('../Middlewares/auth');
 
@@ -12,11 +12,19 @@ router.post('/', auth, CreatePost);
 
 router.get('/',getPosts);
 
+router.get('/user/:userId', getPostsByUser)
+
+router.get('/:id', getPostById)
+
 router.put('/:id', auth, updatePost)
 
 router.patch('/:id', auth, patchPost)
 
 router.delete('/:id', auth, deletePost)
+
+
+
+
 
 
 module.exports = router;
